5c846069445e40bd29df9f5db7ca5d5251d7b001
SameerSundaray <sameersundaray2014@gmail.com>
2024-01-25 12:57:50 +0530

    Add ExpenseItem.js

---
+++ ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ExpenseItem() {
+  return (
+    <div>
+      <p>Food Rs 100</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 300</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
